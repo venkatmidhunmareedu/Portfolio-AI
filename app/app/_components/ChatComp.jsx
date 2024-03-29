470a586a355ca105bf0207f3f49dfd85f193ef23
Midhun-at-083118395813 <Midhun-at-083118395813>
2024-03-29 15:54:42 +0530

    Update ChatComp.jsx

--- app/app/_components/ChatComp.jsx
+++ app/app/_components/ChatComp.jsx
@@ -23,7 +23,7 @@ const ChatComp = (props) => {
     }
     return (
         <div class={"flex" + (props.sender ? " justify-end" : " justify-start")}>
-            <div class={"flex max-w-[50%] flex-col rounded px-3 py-1 md:max-w-[50%]" + (props.sender ? " bg-slate-100" : " bg-blue-100 ")}>
+            <div class={"flex max-w-[75%] flex-col rounded px-3 py-1 md:max-w-[50%]" + (props.sender ? " bg-slate-100" : " bg-blue-100 ")}>
                 <p class="text-left text-slate-900 text-sm">{props.text}</p>
                 <div class="flex justify-end text-xs text-slate-500 mt-1">{props.time}</div>
             </div>
